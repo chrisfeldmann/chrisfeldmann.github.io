ab43d522cebfd8729225c72273f3a323770f21a0
chrisfeldmann <chris@chrisfeldmann.com>
2015-05-20 17:59:54 -0400

    Version 0.0.3
    Huge stride in styling and CSS. Focused on optimization for mobile.

--- js/freelancer.js
+++ js/freelancer.js
@@ -37,7 +37,7 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 
-//CHRIS FELDMANN ADDED : NAV BRAND HIDDEN UNTIL SCROLL
+//CHRIS FELDMANN ADDED : NAV BRAND AND SCROLL TOP BUTTON HIDDEN UNTIL SCROLL TO FIRST SECTION
  //when the DOM has loaded
     $(document).ready(function() {
 
@@ -46,7 +46,7 @@ $('.navbar-collapse ul li a').click(function() {
         $(window).scroll(function () {
               var height = $('html').height() || $('body').scrollTop();
               var scrollTop = $('html').scrollTop() || $('body').scrollTop();
-              var opacity = 1;
+              var opacity =1;
 
               // do some math here, by placing some condition or formula
               if(scrollTop > 500) {
@@ -54,13 +54,13 @@ $('.navbar-collapse ul li a').click(function() {
                   
               }
 
-              if (scrollTop <500){
+              if (scrollTop < 500){
                 opacity = 0;
                
               }
 
-              //set the opacity of div id="someDivId"
-              $('.navbar-fixed-top .navbar-brand').css('opacity', opacity);
+              //set the opacity of div id=".navbar-brand" and ".scroll-top" 
+              $('.navbar-brand').css('opacity', opacity);
               $('.scroll-top').css('opacity', opacity);
               
 
@@ -68,14 +68,16 @@ $('.navbar-collapse ul li a').click(function() {
    
    // });
 
-    // CHRIS FELDMANN ADDED : NAV BAR DEFAULT BACKGROUND COLOR ZERO OPACITY UNTIL FIRST SECTION
+    // CHRIS FELDMANN ADDED : NAV BAR DEFAULT BACKGROUND COLOR ZERO OPACITY UNTIL SCROLL TO FIRST SECTION
 
     $(document).on('scroll', function (e) {
 
-     var alpha = $(document).scrollTop() / 550;
+     var alpha = $(document).scrollTop() / 500;
 
-     $('.navbar-default').css('background-color', 'rgba(44, 62, 80,' + alpha + ')');
-      //$('.navbar-inverse').css('background-color', 'rgba(70, 103, 26,' + alpha + ')');
+     // GREEN COLOR PALETTE NAV BAR BELOW
+     // $('.navbar-default').css('background-color', 'rgba(104, 159, 56,' + alpha + ')');
+     // BLUE GREY COLOR PALETTE NAV BAR BELOW
+      $('.navbar-default').css('background-color', 'rgba(38, 50, 56,' + alpha + ')');
 });
 
     });
